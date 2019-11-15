ecc8c4b6b05c7f661bee12a0ddb7a74d4c7bdf3c
Nathaly <natharb09@gmail.com>
2019-11-15 16:47:24 -0500

    feat: implementing responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/components/Header.scss';
+import menuIcon from '../assets/static/menu.png';
 
-const Header = () => (
-  <header className='header'>
-    <nav className='header__menu'>
-      <ul>
-        <li>
-          <a href='#home'>Inicio</a>
-        </li>
-        <li>
-          <a href='#about'>Sobre Mí</a>
-        </li>
-        <li>
-          <a href='#portfolio'>Proyectos</a>
-        </li>
-        <li>
-          <a href='#contact'>Contacto</a>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [navOpened, setNavOpened] = useState(false);
+  const navClassName = navOpened ? 'header__menu is-active' : 'header__menu';
+
+  return (
+    <header className='header'>
+      <div id='burguer__menu' onClick={() => setNavOpened(!navOpened)}>
+        <img src={menuIcon} alt='Menu' />
+      </div>
+      <nav className={navClassName}>
+        <ul>
+          <li>
+            <a href='#home'>Inicio</a>
+          </li>
+          <li>
+            <a href='#about'>Sobre Mí</a>
+          </li>
+          <li>
+            <a href='#portfolio'>Proyectos</a>
+          </li>
+          <li>
+            <a href='#contact'>Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
